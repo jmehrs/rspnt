ee0aef73815f636fd502f88a8171cf2341a8a052
Jai Mehra <jmjaiken@gmail.com>
2022-11-20 20:36:27 -0500

    Added basic tracing setup & usage in main

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 use clap::Parser;
+use rspnt::telemetry::{get_subscriber, init_subscriber};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, author, about, long_about = None)]
 struct Args {
-   /// Name of the person to greet
-   #[arg(short, long)]
-   name: String,
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
 
-   /// Number of times to greet
-   #[arg(short, long, default_value_t = 1)]
-   count: u8,
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
 }
 
 fn main() {
-   let args = Args::parse();
+    let args = Args::parse();
+    let subscriber = get_subscriber("info");
+    init_subscriber(subscriber);
 
-   for _ in 0..args.count {
-       println!("Hello {}!", args.name)
-   }
+    for _ in 0..args.count {
+        tracing::info!("Hello {}!", args.name)
+    }
 }
